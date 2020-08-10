dec0b1821a513463182160b382594b210528dd15
mihajlo33-dev <61163023+mihajlo33-dev@users.noreply.github.com>
2020-08-10 09:24:59 +0200

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication111.Controllers
+{
+    public class HomeController : Controller
+    {
+        MainLogic.MainLogic Logic = null;
+
+        public HomeController()
+        {
+            Logic = new MainLogic.MainLogic();
+            
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult SnimiPoruku(string Ime, string Email, string GlavnaPoruka)
+        {
+
+            Logic.SnimiPoruku(Ime, Email, GlavnaPoruka);
+
+            return Json("OK", JsonRequestBehavior.AllowGet);
+
+        }
+
+        [HttpPost]
+        public JsonResult SnimiEmail(string email1)
+        {
+
+            Logic.SnimiEmail(email1);
+
+            return Json("GREAT", JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
