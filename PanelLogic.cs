80a6122bb9e8ea905642353dd9e3a8a5e6c33500
mihajlo33-dev <61163023+mihajlo33-dev@users.noreply.github.com>
2020-08-10 09:12:27 +0200

    Add files via upload

---
+++ PanelLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication111.Models.panel
+{
+
+
+
+
+    public class VratiAgenciju : Controller
+    {
+        private agencijaEntities db = new agencijaEntities();
+
+        public ActionResult Index()
+        {
+            return View(db.agencijas.ToList());
+        }
+
+
+    }
+}
